87c03b3875d30d5b48503c05caea848654af6e30
Ariel Zusman <ariel.zusman@gmail.com>
2024-01-10 02:46:57 +0200

    add translation and json viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,22 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {InputTextModule} from "primeng/inputtext";
 import {MultiSelectModule} from "primeng/multiselect";
 import {TreeModule} from "primeng/tree";
+import {NgxJsonViewerModule} from "ngx-json-viewer";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        DropdownModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule,
-        InputTextModule,
-        MultiSelectModule,
-        TreeModule
-    ],
+  imports: [
+    BrowserModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    MultiSelectModule,
+    TreeModule,
+    NgxJsonViewerModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
